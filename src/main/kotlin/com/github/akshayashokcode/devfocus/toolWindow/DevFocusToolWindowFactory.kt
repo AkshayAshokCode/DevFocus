8ee22d1985e1a385afe2de38be8e66c70398d686
akshay ashok <akshayashokan1054@gmail.com>
2025-06-29 18:38:59 +0530

    #7 Design Basic Tool Window UI

--- src/main/kotlin/com/github/akshayashokcode/devfocus/toolWindow/DevFocusToolWindowFactory.kt
+++ src/main/kotlin/com/github/akshayashokcode/devfocus/toolWindow/DevFocusToolWindowFactory.kt
@@ -1,24 +1,16 @@
 package com.github.akshayashokcode.devfocus.toolWindow
 
-import com.intellij.openapi.diagnostic.thisLogger
+import com.github.akshayashokcode.devfocus.ui.PomodoroPanel
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
-import javax.swing.JLabel
-import javax.swing.JPanel
 
 
 class DevFocusToolWindowFactory : ToolWindowFactory {
 
-    init {
-        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
-    }
-
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val panel = JPanel()
-        panel.add(JLabel("Pomodoro Timer"))
-
+        val panel = PomodoroPanel()
         val content = ContentFactory.getInstance().createContent(panel, "", false)
         toolWindow.contentManager.addContent(content)
     }
